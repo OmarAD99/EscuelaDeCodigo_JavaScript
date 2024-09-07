95ae9710bd2daea9d5700da94c360c39e7774282
OmarAD99 <RockLeeAOELH@gmail.com>
2024-09-07 14:57:25 -0600

    First Commit

---
+++ assets/Scripts/Ejemplo1.js
@@ -0,0 +1,4 @@
+/* Crea 3 rondas de Periodos de Calificación y saca la suma de todos, dividelas entre 3 y de ésas 3 que salieron, vuelvelas a sumar y vuelvelas a dividir para sacar la Calificación final con los Bucles While y For */
+
+var Calcular_Promedios = document.getElementById("Calcular-Promedios");
+
---
+++ assets/Scripts/Ejemplo2.js
@@ -0,0 +1,2 @@
+/* Crea 3 rondas de Periodos de Calificación y saca la suma de todos, dividelas entre 3 y de ésas 3 que salieron, vuelvelas a sumar y vuelvelas a dividir para sacar la Calificación final con los Bucles While y For */
+
